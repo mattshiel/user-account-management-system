a8d556f2bc3b5f3d756a0e2a513667f0f15d3d98
mattshiel <mattshiel@gmail.com>
2019-11-29 16:57:53 +0000

    Seperate database
    Organise the hashmap into its own database class and populate the map with test users, rebuild the jar and test locally with a get request to retrieve all users from the 'database'

--- user-account-service/src/main/java/ie/gmit/ds/api/UserApiResource.java
+++ user-account-service/src/main/java/ie/gmit/ds/api/UserApiResource.java
@@ -2,7 +2,7 @@ package ie.gmit.ds.api;
 
 import java.util.Collection;
 import java.util.HashMap;
-
+import ie.gmit.ds.database.UserDB;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -16,13 +16,12 @@ public class UserApiResource {
 	private HashMap<Integer, User> usersMap = new HashMap<Integer, User>();
 
 	public UserApiResource() {
-		User testUser = new User(1, "user1", "user1@email.com", "password");
-		usersMap.put(testUser.getUserId(), testUser);
+		
 	}
 
 	@GET
 	public Collection<User> getUsers() {
 		// Return users
-		return usersMap.values();
+		return UserDB.getUsers();
 	}
 }
